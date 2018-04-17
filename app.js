7df4bb5f89ae14c30f4f2a50f72004a517367648
Iam Slaughter <jlgslaughter@gmail.com>
2018-04-17 13:18:04 -0400

    my first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const exphbs= require('express-handlebars')
+
+const app = express();
+
+//Handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//Index Route
+app.get('/', (req, res)=>{
+    const title = 'Welcome'
+    res.render('index', {
+        title: title
+    })
+})
+
+app.get('/about', (req, res)=>{
+    res.render('about')
+})
+
+const port = 8080;
+
+app.listen(8080, ()=> {
+    console.log(`server started on port${port}`)
+})
